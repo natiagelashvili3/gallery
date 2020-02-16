81c3ed24c3b9662f7b70ae81b7625dbcdb7746e3
natiagelashvili <n_gelashvili3@cu.edu.ge>
2020-02-16 15:34:52 +0400

    change script

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// var gallery = document.getElementsByClassName('gallery-item');
-
-// function setImage(img_src) {
-//     document.getElementById('gallery-main').getElementsByTagName('img')[0].setAttribute("src", img_src)
-// }
-
-// function init() {
-//     setImage(gallery[0].getElementsByTagName('img')[0].getAttribute('src'))
-// }
-// init();
-
-// for (let i = 0; i < gallery.length; i++) {
-//     gallery[i].addEventListener('click', function() {
-//         var img_src = gallery[i].getElementsByTagName('img')[0].getAttribute('src')
-//         setImage(img_src)
-//     }) 
-// }
-
-
 
 var gallery_item = document.getElementsByClassName('gallery-item')
 var gallery_main_div = document.getElementById('gallery-main');
